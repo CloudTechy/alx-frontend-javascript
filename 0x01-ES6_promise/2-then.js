617900f30f7c843c541f2a460b5da47d7c8f13b9
Onyekelu (Chuks) Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-07-29 13:31:29 +0100

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,18 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-    return promise
-        .then(() => {
-            // Return the object with status and body on resolution
-            return {
-                status: 200,
-                body: 'success'
-            };
-        })
-        .catch(() => {
-            // Return an empty Error object on rejection
-            return new Error();
-        })
-        .finally(() => {
-            // Log the message after Promise is settled
-            console.log('Got a response from the API');
-        });
+  const response = { status: 200, body: 'success' };
+  return promise
+    .then(() => response)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
 }
