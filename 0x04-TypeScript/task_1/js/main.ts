18c100c33cbc272c7d7d40d5658f10375443248b
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-07-31 21:11:49 +0100

    done

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,4 +15,60 @@ const teacher1: Teacher = {
     contract: true
 }
 
-console.log(teacher1);
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+function printTeacher(teacher: Teacher): string {
+    return `${teacher.firstName[0].toUpperCase()}. ${teacher.lastName}`;
+}
+
+interface Student {
+    firstName: string;
+    lastName: string;
+ }
+
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+
+    constructor({firstName, lastName}: Student) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    };
+    
+    /**
+     * workOnHomework
+     * @return {string}
+     * @description Returns the status of the student working on homework
+     */
+    public workOnHomework(): string {
+        return 'Currently working';
+    }
+    
+    /**
+     * displayName
+     * @return {string}
+     * @description Returns the full name of the student
+     * @example
+     * const student = new StudentClass('John', 'Doe');
+     * student.displayName(); // John
+     * @returns {string}
+     */
+    public displayName(): string {
+        return this.firstName;
+    }
+}
+
+// Example usage
+const student = new StudentClass({ firstName: "John", lastName: "Doe" });
+console.log(student.displayName()); // Output: John 
+console.log(student.workOnHomework()); // Output: Currently working
---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,87 @@
+// Create the DirectorInterface interface with the 3 expected methods:
+//     - workFromHome
+//     - getCoffeeBreak
+//     - workDirectorTasks
+
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+// Create the Director class implementing the DirectorInterface interface with the 3 expected methods:
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return 'Working from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break';
+    }
+
+    workDirectorTasks(): string {
+        return 'Getting to director tasks';
+    }
+}
+
+// Create the Teacher class implementing the TeacherInterface interface with the 3 expected methods:
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+
+    workTeacherTasks(): string {
+        return 'Getting to work';
+    }
+}
+
+// Create the createEmployee function that will return a new Director or Teacher object based on the type of employee passed to it:
+
+function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    } else {
+        return new Director();
+    }
+}
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
+
+function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork( employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+    
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
+const Subjects: string[] = ['Math', 'English', 'History', 'Science'];
+
+function teachClass(todayClass: string): string {
+    if (Subjects.includes(todayClass)) {
+        return `Teaching ${todayClass}`;
+    }
+}
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
